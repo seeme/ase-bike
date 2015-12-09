326171ae2969e558b5d252a8c0662b929216a0e7
Student A <seeme.goo@gmail.com>
2015-12-09 20:36:16 +0800

    add junit

---
+++ src/test/java/fcu/ase/service/BikeServiceTest.java
@@ -0,0 +1,15 @@
+package fcu.ase.service;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class BikeServiceTest {
+  
+  @Test
+  public void  testHello()
+  {
+    Assert.assertEquals("a", "b");
+  }
+
+}
